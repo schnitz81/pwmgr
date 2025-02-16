0eca4f757c074a623b45b21caf7eeef27720856e
schnitz81 <k.quick81@gmail.com>
2025-02-16 01:22:52 +0100

    dump to unencrypted db file option added

--- process.py
+++ process.py
@@ -70,8 +70,8 @@ def interpret_and_process(base64_stringdata):
 
         # only create new DB if nonew isn't set
         if nonew and not file.file_exists(f'{config.db_path}/{sessionuser}.encdb'):
-            print("Error: nonew selected and user DB doesn't exist.")
-            returnmsg = "1 Error: nonew selected and user DB doesn't exist."
+            print(f"Error: nonew selected and user DB ({config.db_path}/{sessionuser}.encdb) doesn't exist.")
+            returnmsg = f"1 nonew selected and user DB ({config.db_path}/{sessionuser}.encdb) doesn't exist."
             return returnmsg
 
         # Connect to and create DB if non-existent
@@ -82,28 +82,28 @@ def interpret_and_process(base64_stringdata):
 
         # verify connection
         if not connected_to_db(conn):
-            returnmsg = "1 User exists in server, but failed to decrypt and connect to existing DB. Possibly wrong password."
+            returnmsg = f"1 Unable to decrypt and connect to existing server DB ({config.db_path}/{sessionuser}.encdb). Possibly wrong password."
             return returnmsg
 
         # check if credentials exist
         if database.credentials_exist(conn):
             # check if received credentials match db
             if database.credentials_match(conn, sessionuser, sessionpw):
-                returnmsg = "2 Credentials match previous record. No change needed in server."
+                returnmsg = f"2 Credentials match previous record. Reusing server DB for '{sessionuser}'."
             # received credentials don't match
             else:
-                returnmsg = "1 Error: User exists in server and provided password is wrong."
+                returnmsg = f"1 User DB for '{sessionuser}' exists in server but provided password is wrong."
 
         # credentials don't exist since before
         else:
             credentials_stored = database.store_credentials(conn, sessionuser, sessionpw)
             db_written = database.write_inmem_db_to_file(conn, sessionuser, sessionpw)  # write encrypted db file
             if not db_written:
-                returnmsg = "1 Error: unable to write DB to disk."
+                returnmsg = f"1 Unable to write server DB to disk ({config.db_path}/{sessionuser}.encdb)."
             elif credentials_stored:
-                returnmsg = "2 No previous credentials for user in DB. Saving."
+                returnmsg = f"2 No previous credentials for user '{sessionuser}' in DB. Saving."
             else:
-                returnmsg = "1 Error: Credentials storing unsuccessful."
+                returnmsg = "1 Credentials storing unsuccessful."
         database.close_connection(conn)
         return returnmsg
 
@@ -122,7 +122,7 @@ def interpret_and_process(base64_stringdata):
 
         # verify db existence
         if not file.file_exists(f'{config.db_path}/{sessionuser}.encdb'):
-            returnmsg = "1 Error: DB doesn't exist. Unable to change previous session. Create a new session and DB with init."
+            returnmsg = f"1 DB file ({config.db_path}/{sessionuser}.encdb) doesn't exist in server. Client and server session not aligned."
             return returnmsg
         else:
             print("DB exists.")
@@ -135,7 +135,7 @@ def interpret_and_process(base64_stringdata):
 
         # verify connection and session credentials
         if not connected_to_db(conn):
-            returnmsg = "1 Unable to decrypt and connect to existing DB. Possibly wrong password. Current session user and password must be verified to change them."
+            returnmsg = f"1 Unable to decrypt and connect to existing DB ({config.db_path}/{sessionuser}.encdb). Possibly wrong password. Current session user and password must be verified to change them."
             return returnmsg
         elif not database.credentials_match(conn, sessionuser, sessionpw):
             returnmsg = "1 Entered session credentials don't match DB records. Current session user and password must be verified to change them."
@@ -158,13 +158,13 @@ def interpret_and_process(base64_stringdata):
 
             # return rename and credentials change result
             if credentials_stored and dbfile_renamed and db_written:
-                returnmsg = "2 Old credentials overwritten and db file renamed successfully."
+                returnmsg = f"2 Old credentials overwritten and db file renamed successfully ({sessionuser} -> {sessionnewuser})."
             elif not credentials_stored:
-                returnmsg = "1 Error: Credentials storing unsuccessful."
+                returnmsg = "1 Credentials storing unsuccessful."
             elif credentials_stored and not dbfile_renamed:
-                returnmsg = "1 Error: Credentials stored but unable to rename DB file. Login will probably not work unless DB file is renamed."
+                returnmsg = "1 Credentials stored but unable to rename DB file. Login will probably not work unless DB file is renamed."
             elif not db_written:
-                returnmsg = "1 Error: unable to write changed DB to disk."
+                returnmsg = "1 Unable to write changed DB to disk."
             return returnmsg
 
     ### status ##########################################################################################
@@ -179,7 +179,7 @@ def interpret_and_process(base64_stringdata):
 
         # verify db existence
         if not file.file_exists(f'{config.db_path}/{sessionuser}.encdb'):
-            returnmsg = "1 Error: DB doesn't exist. Client and server session not aligned."
+            returnmsg = f"1 DB file ({config.db_path}/{sessionuser}.encdb) doesn't exist in server. Client and server session not aligned."
             return returnmsg
         else:
             print("DB exists.")
@@ -189,12 +189,12 @@ def interpret_and_process(base64_stringdata):
 
         # verify connection and session credentials
         if not connected_to_db(conn):
-            returnmsg = "1 Unable to decrypt and connect to existing DB. Possibly wrong password."
+            returnmsg = f"1 Unable to decrypt and connect to existing server DB ({config.db_path}/{sessionuser}.encdb). Possibly wrong password."
             return returnmsg
         elif not database.credentials_match(conn, sessionuser, sessionpw):
-            returnmsg = "1 Session credentials don't match DB."
+            returnmsg = f"1 Session credentials don't match server DB file ({config.db_path}/{sessionuser}.encdb)."
         else:
-            returnmsg = "2 Session credentials received match server DB."
+            returnmsg = f"2 Success: Session check successful against server DB ({config.db_path}/{sessionuser}.encdb)."
         database.close_connection(conn)
         return returnmsg
 
@@ -216,7 +216,7 @@ def interpret_and_process(base64_stringdata):
 
         # verify db existence
         if not file.file_exists(f'{config.db_path}/{sessionuser}.encdb'):
-            returnmsg = "1 Error: DB doesn't exist. Client and server session not aligned."
+            returnmsg = f"1 DB file ({config.db_path}/{sessionuser}.encdb) doesn't exist in server. Client and server session not aligned."
             return returnmsg
         else:
             print("DB exists.")
@@ -237,19 +237,19 @@ def interpret_and_process(base64_stringdata):
 
         # if letter is missing in title
         if not any(c.isalpha() for c in title):
-            returnmsg = "1 Error: Invalid title name. At least one letter is required."
+            returnmsg = "1 Invalid title name. At least one letter is required."
 
         # "all" is reserved.
         elif title.casefold() == "all":
-            returnmsg = "1 Error: Invalid title name. \"ALL\" is reserved."
+            returnmsg = "1 Invalid title name. \"ALL\" is reserved."
 
         # avoid overwrite when add command is used
         elif database.exact_title_exists(conn, title) and command != 'update':
-            returnmsg = "1 Error: Record already exists. Use update to overwrite/change record."
+            returnmsg = "1 Record already exists. Use update to overwrite/change record."
 
         # don't create new record with update command if record is missing
         elif not database.exact_title_exists(conn, title) and command == 'update':
-            returnmsg = "1 Error: Record not found. Unable to update. Specify exact name to update."
+            returnmsg = "1 Record not found. Unable to update. Specify exact name to update."
 
         else:
             # get exact title case spelling when using update
@@ -257,7 +257,7 @@ def interpret_and_process(base64_stringdata):
                 title = database.get_title_case_spelling(conn, title)
 
             if not title:  # not able to check title case spelling
-                returnmsg = "1 Error: Record storing unsuccessful. Unable to get title case spelling."
+                returnmsg = "1 Record storing unsuccessful. Unable to get title case spelling."
 
             # store record
             else:
@@ -265,13 +265,13 @@ def interpret_and_process(base64_stringdata):
                 if record_stored:
                     db_written = database.write_inmem_db_to_file(conn, sessionuser, sessionpw)  # write encrypted db file
                     if not db_written:
-                        returnmsg = "1 Error: unable to write changed DB to disk."
+                        returnmsg = "1 Unable to write changed DB to disk."
                     elif command == 'add':  # return message depending on command used
                         returnmsg = "2 Record stored in DB successfully."
                     else:  # update command used
                         returnmsg = "2 Record updated in DB successfully."
                 else:
-                    returnmsg = "1 Error: Record storing unsuccessful."
+                    returnmsg = "1 Record storing unsuccessful."
         database.close_connection(conn)
         return returnmsg
 
@@ -289,7 +289,7 @@ def interpret_and_process(base64_stringdata):
 
         # verify db existence
         if not file.file_exists(f'{config.db_path}/{sessionuser}.encdb'):
-            returnmsg = "1 Error: DB doesn't exist. Client and server session not aligned."
+            returnmsg = f"1 DB file ({config.db_path}/{sessionuser}.encdb) doesn't exist in server. Client and server session not aligned."
             return returnmsg
         else:
             print("DB exists.")
@@ -310,7 +310,7 @@ def interpret_and_process(base64_stringdata):
 
         # if letter is missing in title
         if not any(c.isalpha() for c in title):
-            returnmsg = "1 Error: Invalid title name. At least one letter is required."
+            returnmsg = "1 Invalid title name. At least one letter is required."
 
         # get record, exact match or multimatch suggestions
         elif database.exact_title_exists(conn, title):
@@ -340,7 +340,7 @@ def interpret_and_process(base64_stringdata):
 
         # verify db existence
         if not file.file_exists(f'{config.db_path}/{sessionuser}.encdb'):
-            returnmsg = "1 Error: DB doesn't exist. Client and server session not aligned."
+            returnmsg = f"1 DB file ({config.db_path}/{sessionuser}.encdb) doesn't exist in server. Client and server session not aligned."
             return returnmsg
         else:
             print("DB exists.")
@@ -361,7 +361,7 @@ def interpret_and_process(base64_stringdata):
 
         # if letter is missing in title
         if not any(c.isalpha() for c in title):
-            returnmsg = "1 Error: Invalid title name. At least one letter is required."
+            returnmsg = "1 Invalid title name. At least one letter is required."
 
         # if list all is requested
         elif title.casefold() == 'all'.casefold():
@@ -393,7 +393,7 @@ def interpret_and_process(base64_stringdata):
 
         # verify db existence
         if not file.file_exists(f'{config.db_path}/{sessionuser}.encdb'):
-            returnmsg = "1 Error: DB doesn't exist. Client and server session not aligned."
+            returnmsg = f"1 DB file ({config.db_path}/{sessionuser}.encdb) doesn't exist in server. Client and server session not aligned."
             return returnmsg
         else:
             print("DB exists.")
@@ -414,14 +414,14 @@ def interpret_and_process(base64_stringdata):
 
         # if letter is missing in title
         if not any(c.isalpha() for c in title):
-            returnmsg = "1 Error: Invalid title name. At least one letter is required."
+            returnmsg = "1 Invalid title name. At least one letter is required."
 
         # delete exact match
         elif database.exact_title_exists(conn, title):
             record_deleted = database.delete_record(conn, title)
             db_written = database.write_inmem_db_to_file(conn, sessionuser, sessionpw)  # write encrypted db file
             if not db_written:
-                returnmsg = "1 Error: unable to write changed DB to disk."
+                returnmsg = "1 Unable to write changed DB to disk."
             elif record_deleted:
                 returnmsg = "2 Record deleted."
             else:
@@ -437,6 +437,44 @@ def interpret_and_process(base64_stringdata):
             returnmsg = "1 Unknown server error."
         database.close_connection(conn)
         return returnmsg
+
+
+    ### backup ##########################################################################################
+    elif command == 'backup':
+        try:
+            sessionuser = base64.b64decode(base64.b64decode(decompressed_data.split(' ')[1])).decode('utf8').rstrip()
+            sessionpw = base64.b64decode(base64.b64decode(decompressed_data.split(' ')[2])).decode('utf8').rstrip()
+        except Exception as b64decode_error:
+            print(f"Error: Unable to decode base64 data: {b64decode_error}")
+            returnmsg = "1 Invalid base64 data to decode."
+            return returnmsg
+
+        # verify db existence
+        if not file.file_exists(f'{config.db_path}/{sessionuser}.encdb'):
+            returnmsg = f"1 Encrypted DB file ({config.db_path}/{sessionuser}.encdb) not found in server. Unable to dump DB into backup file."
+            return returnmsg
+        else:
+            print("DB exists.")
+
+        # connect to db
+        conn = database.create_connection(sessionuser, sessionpw)
+
+        # verify connection and session credentials
+        if not connected_to_db(conn):
+            returnmsg = "1 Unable to decrypt and connect to existing DB. Possibly wrong password."
+            return returnmsg
+        elif not database.credentials_match(conn, sessionuser, sessionpw):
+            returnmsg = "1 Session credentials don't match DB."
+        else:
+            # dump DB into unencrypted file
+            if database.write_inmem_db_to_file_unencrypted(conn, sessionuser):
+                returnmsg = f"2 Database successfully backed up to {config.db_path}/{sessionuser}.db in server."
+            else:
+                returnmsg = f"1 Unable to backup database as unencrypted to {config.db_path}/{sessionuser}.db in server."
+
+        database.close_connection(conn)
+        return returnmsg
+
 
     ### no command matches ###############################################################################
     else:
